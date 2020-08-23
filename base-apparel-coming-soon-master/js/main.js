930cb0a1fc4f8f4b7bbb45927e7cac529a6dcde9
EdwinPilePrazeres <69055818+EdwinPilePrazeres@users.noreply.github.com>
2020-08-23 07:53:20 -0700

    Add files via upload

---
+++ base-apparel-coming-soon-master/js/main.js
@@ -0,0 +1,9 @@
+
+
+  var submit = document.querySelector(".button-position");
+  var atencion = document.querySelector(".error");
+
+  submit.addEventListener("click", function(){
+    atencion.style.display="block";
+    
+  })
